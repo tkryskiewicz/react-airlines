0e2fd76b5666aefcb80ada8114ee41a9ac189d72
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2018-06-05 15:11:07 +0200

    setup unit tests (#22)
    * setup Jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+  ],
+  testMatch: [
+    "**/*.test.(ts|tsx)",
+  ],
+  transform: {
+    "\\.(ts|tsx)$": "ts-jest",
+  },
+};
---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import { App } from "./App";
+
+describe("App", () => {
+  it("should exist", () => {
+    expect(<App />).toBeDefined();
+  });
+});
