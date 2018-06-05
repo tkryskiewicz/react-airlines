9b9255b308d36a6fabd6a768e3208e13be053064
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2018-06-05 12:04:08 +0200

    setup main page (#14)

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,4 +1,5 @@
 import * as Webpack from "webpack";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
 
 const config: Webpack.Configuration = {
   entry: "./src",
@@ -11,6 +12,11 @@ const config: Webpack.Configuration = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
   resolve: {
     extensions: [
       ".ts",
