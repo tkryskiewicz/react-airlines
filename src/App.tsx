b952f5ec9feefc6585e32cc25f4e77b84058bdd1
Tomasz Kryśkiewicz <contact@tkryskiewicz.com>
2018-06-07 13:45:58 +0200

    implement layout (#30)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
+import { Layout } from "antd";
 import * as React from "react";
 
 export class App extends React.Component {
   public render() {
     return (
       <>
-        <h1>
-          react-airlines
-        </h1>
-        <p>
-          An airlines website created using TypeScript and React.
-        </p>
+        <Layout>
+          <Layout.Header>
+            <a
+              style={{ color: "#fff" }}
+              href="/"
+            >
+              react-airlines
+            </a>
+          </Layout.Header>
+          <Layout.Content>
+            <h1>
+              react-airlines
+            </h1>
+            <p>
+              An airlines website created using TypeScript and React.
+            </p>
+          </Layout.Content>
+          <Layout.Footer>
+            2018 &copy; react-airlines. All rights reserved.
+          </Layout.Footer>
+        </Layout>
       </>
     );
   }
