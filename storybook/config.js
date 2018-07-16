1b98b1587b57024bb641bdefbbb9fc4c4d6a577d
Tomasz Kryskiewicz <tomasz.kryskiewicz@tkryskiewicz.com>
2018-07-16 08:41:03 +0200

    extract Storybook addons config and convert to TypeScript

--- storybook/config.js
+++ storybook/config.js
@@ -1,36 +1,8 @@
-import { withKnobs } from "@storybook/addon-knobs";
 import { configure, addDecorator } from "@storybook/react";
 
 import "antd/dist/antd.css";
 
-addDecorator(withKnobs);
-
-import { addLocaleData } from "react-intl";
-
-import enLocaleData from "react-intl/locale-data/en";
-import plLocaleData from "react-intl/locale-data/pl";
-
-addLocaleData([
-  ...enLocaleData,
-  ...plLocaleData,
-]);
-
-import { setIntlConfig, withIntl } from "storybook-addon-intl";
-
-setIntlConfig({
-  defaultLocale: "en",
-  getMessages: () => ({}),
-  locales: [
-    "en",
-    "pl",
-  ],
-});
-
-addDecorator(withIntl);
-
-import { withAntdLocale } from "./withAntdLocale";
-
-addDecorator(withAntdLocale);
+import "./addons-config";
 
 const req = require.context("../src", true, /\.story\.tsx$/);
 
